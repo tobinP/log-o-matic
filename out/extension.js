b53bc76f103d21c9e54e3bad24d6fe0f4d55b8da
Tobin <tobin.pomeroy@gmail.com>
2021-07-12 11:40:51 -0700

    Adds support for dotnet and js

--- out/extension.js
+++ out/extension.js
@@ -1,6 +1,23 @@
 "use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
 Object.defineProperty(exports, "__esModule", { value: true });
+exports.deactivate = exports.activate = void 0;
 const vscode = require("vscode");
+var FileType;
+(function (FileType) {
+    FileType[FileType["Unity"] = 0] = "Unity";
+    FileType[FileType["Dotnet"] = 1] = "Dotnet";
+    FileType[FileType["Js"] = 2] = "Js";
+    FileType[FileType["Unknown"] = 3] = "Unknown";
+})(FileType || (FileType = {}));
 function activate(context) {
     let disposable = vscode.commands.registerCommand("extension.log", () => {
         const editor = vscode.window.activeTextEditor;
@@ -12,14 +29,48 @@ function activate(context) {
 }
 exports.activate = activate;
 function doCommand(editor) {
-    const selectedText = editor.document.getText(editor.selection);
-    const isVariable = checkIfVariable(editor, editor.selection);
-    skipOverOpenCurly(editor);
-    vscode.commands.executeCommand("editor.action.insertLineAfter").then(() => {
+    return __awaiter(this, void 0, void 0, function* () {
+        const selectedText = editor.document.getText(editor.selection);
+        const isVariable = checkIfVariable(editor, editor.selection);
+        const fileType = yield getFileType(editor);
+        if (fileType === FileType.Unknown)
+            return;
+        skipOverOpenCurly(editor);
+        yield vscode.commands.executeCommand("editor.action.insertLineAfter");
         editor.edit(editBuilder => {
-            addLog(editor, editBuilder, selectedText, isVariable);
-            importUnity(editor, editBuilder);
+            switch (fileType) {
+                case FileType.Unity:
+                    addLogStatement(editor, editBuilder, "Debug.Log", selectedText, isVariable);
+                    addUnityImportStatement(editor, editBuilder);
+                    break;
+                case FileType.Dotnet:
+                    addLogStatement(editor, editBuilder, "Console.WriteLine", selectedText, isVariable);
+                    break;
+                case FileType.Js:
+                    addLogStatement(editor, editBuilder, "console.log", selectedText, isVariable);
+                    break;
+                default:
+                    break;
+            }
         });
+    });
+}
+function getFileType(editor) {
+    return __awaiter(this, void 0, void 0, function* () {
+        let langId = editor.document.languageId;
+        if (langId === 'typescript' || langId === 'javascript')
+            return Promise.resolve(FileType.Js);
+        if (langId === 'csharp') {
+            let files = yield vscode.workspace.findFiles('packages/manifest.json', undefined, 1);
+            if (files.length === 0) {
+                return Promise.resolve(FileType.Dotnet);
+            }
+            else {
+                return Promise.resolve(FileType.Unity);
+            }
+        }
+        vscode.window.showInformationMessage(`Unsupported file type`);
+        return Promise.resolve(FileType.Unknown);
     });
 }
 function checkIfVariable(editor, selection) {
@@ -38,17 +89,17 @@ function skipOverOpenCurly(editor) {
         editor.selection = selection;
     }
 }
-function addLog(editor, editBuilder, text, isVariable) {
+function addLogStatement(editor, editBuilder, prefix, text, isVariable) {
     let logStatement;
     if (isVariable) {
-        logStatement = `Debug.Log("&&& ${text}: " + ${text});`;
+        logStatement = `${prefix}("&&& ${text}: " + ${text});`;
     }
     else {
-        logStatement = `Debug.Log("&&& ${text}");`;
+        logStatement = `${prefix}("&&& ${text}");`;
     }
     editBuilder.insert(new vscode.Position(editor.selection.active.line, editor.selection.active.character), logStatement);
 }
-function importUnity(editor, editBuilder) {
+function addUnityImportStatement(editor, editBuilder) {
     const importStatement = "using UnityEngine;";
     const documentText = editor.document.getText();
     if (documentText.includes(importStatement))
